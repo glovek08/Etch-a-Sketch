ff6630d711ce17eff7aac2cf7d58c367735dcd57
Gabriel Barn <gbrielbbarn@gmail.com>
2024-03-29 00:15:25 -0300

    [feat] onmouseover drawing

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -6,8 +6,11 @@ function createSquare(i) {
     if (i < 256) {
         let square = document.createElement('div');
         square.classList.add('flex-square');
+        square.addEventListener('mouseover', (e) => {
+            e.target.classList.add('red-bg');
+        });
         squaresContainer.appendChild(square);
-        console.log('calling crateSquare with'+i);
+        console.log('calling crateSquare with: '+i);
         createSquare(i + 1);
     }
     return;
