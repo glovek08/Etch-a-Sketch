615fbc1dd71be4e08709b38bf83be3a04fea2e80
Gabriel Barn <gbrielbbarn@gmail.com>
2024-04-01 21:32:15 -0300

    [feat] working new grid function now takes width, deleteGrid function.

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -1,28 +1,52 @@
-const squaresContainer = document.querySelector('#flex-container');
+const squaresContainer = document.querySelector('#grid-container');
 const resetGridBtn = document.querySelector('#reset-grid-btn');
+const popupDiv = document.querySelector('#new-grid-popup');
+const newGridSubmitBtn = document.querySelector('#submit-new-grid');
+
 const DEFAULT_BG = '#FF3C38'; // Vermillion.
 
-createSquare(0);
-resetGridBtn.addEventListener('click', () => {
-    resetGrid();
+
+createSquare(0, 8); //Initialize with 0 and 8
+
+resetGridBtn.addEventListener('click', () => resetGrid());
+
+popupDiv.addEventListener('click', () => {
+    const popup = document.querySelector('#popup-text-span'); //Could use event to hide when mouse is out.
+    popup.classList.add("show");
 });
 
-function createSquare(i) {
-    if (i < 256) {
-        let square = document.createElement('div');
+newGridSubmitBtn.addEventListener('click', () => {
+    const userInputWidth = document.querySelector('#grid-width').value;
+    console.log(`User Input Width: ${userInputWidth}, --grid-width: ${getComputedStyle(document.documentElement).getPropertyValue('--grid-width')}`);
+    document.documentElement.style.setProperty('--grid-width', userInputWidth);
+    console.log(`--grid-width now: ${getComputedStyle(document.documentElement).getPropertyValue('--grid-width')}`);
+    deleteGrid();
+    createSquare(0, userInputWidth);
+});
+
+function createSquare(i, width) {
+    if (i < width * width) {
+        const square = document.createElement('div');
         square.classList.add('flex-square');
         square.addEventListener('mouseover', (e) => {
             e.target.classList.add('red-bg');
         });
         squaresContainer.appendChild(square);
-        console.log('calling crateSquare with: '+i);
-        createSquare(i + 1);
+        console.log('calling createSquare with: ' + i);
+        createSquare(i + 1, width);
     }
     return;
 }
+
+function deleteGrid() {
+    const squareItems = squaresContainer.querySelectorAll('.flex-square');
+    squareItems.forEach(el => squaresContainer.removeChild(el));
+    console.log('Grid Deleted');
+}
 function resetGrid() {
-    let squareItems = squaresContainer.querySelectorAll('.flex-square');
+    const squareItems = squaresContainer.querySelectorAll('.flex-square');
     squareItems.forEach(e => e.classList.remove('red-bg'));
+    console.log('Grid Reset.')
 }
 
 //TODO: Create button that will prompt the use for a new grid and will reset the current grid to the specified one.
