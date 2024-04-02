a3864c6e0cd2077e58e8f35bee80e669ba8b4691
Gabriel Barn <gbrielbbarn@gmail.com>
2024-04-01 22:03:31 -0300

    [feat] display/hide function for new-grid-popup

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -1,19 +1,15 @@
 const squaresContainer = document.querySelector('#grid-container');
 const resetGridBtn = document.querySelector('#reset-grid-btn');
-const popupDiv = document.querySelector('#new-grid-popup');
 const newGridSubmitBtn = document.querySelector('#submit-new-grid');
-
+const popup = document.querySelector('#popup-text-span');
+const test = document.querySelector('#test');
 const DEFAULT_BG = '#FF3C38'; // Vermillion.
 
 
-createSquare(0, 8); //Initialize with 0 and 8
+createSquares(0, 8); //Initialize with 0 and 8
 
 resetGridBtn.addEventListener('click', () => resetGrid());
-
-popupDiv.addEventListener('click', () => {
-    const popup = document.querySelector('#popup-text-span'); //Could use event to hide when mouse is out.
-    popup.classList.add("show");
-});
+test.addEventListener('click', () => newGridPopup(true));
 
 newGridSubmitBtn.addEventListener('click', () => {
     const userInputWidth = document.querySelector('#grid-width').value;
@@ -21,23 +17,35 @@ newGridSubmitBtn.addEventListener('click', () => {
     document.documentElement.style.setProperty('--grid-width', userInputWidth);
     console.log(`--grid-width now: ${getComputedStyle(document.documentElement).getPropertyValue('--grid-width')}`);
     deleteGrid();
-    createSquare(0, userInputWidth);
+    createSquares(0, userInputWidth);
+    newGridPopup(false);
 });
 
-function createSquare(i, width) {
-    if (i < width * width) {
+function createSquares(i, width) {
+    if (i < Math.pow(width, 2)) {
         const square = document.createElement('div');
         square.classList.add('flex-square');
         square.addEventListener('mouseover', (e) => {
             e.target.classList.add('red-bg');
         });
         squaresContainer.appendChild(square);
-        console.log('calling createSquare with: ' + i);
-        createSquare(i + 1, width);
+        createSquares(i + 1, width);
     }
+    console.log(`Grid Created, Size: ${Math.pow(width, 2)}`);
     return;
 }
 
+function newGridPopup(option) { //true to show, false to hide.
+    if (option) {
+        popup.classList.add("show");
+        console.log('Popup show');  
+    }
+    else {
+        popup.classList.remove("show");
+        console.log('Popup hide');
+    } 
+    //Could use event to hide when mouse is out.
+}
 function deleteGrid() {
     const squareItems = squaresContainer.querySelectorAll('.flex-square');
     squareItems.forEach(el => squaresContainer.removeChild(el));
@@ -49,8 +57,9 @@ function resetGrid() {
     console.log('Grid Reset.')
 }
 
-//TODO: Create button that will prompt the use for a new grid and will reset the current grid to the specified one.
-//For the randomizer we could use Math.random to generate a new rgb code.
-/*For the new grid size we could use the current createSquare function to define a new size, instead of 256 we need
-to specify the result of the desired grid to the power of 2 (Math.pow(base, exp)) */
-//Instead of a prompt we could create a popup div.
+/*TODO:
+    For the randomizer we could use Math.random to generate a new rgb code.
+    Style for the popup div and buttons.
+    Control the input, e.g. No more than 50 for the grid size.
+    Remove logs.
+*/
